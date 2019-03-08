a37232281749287e80ab67e8f2258caacc56d4a9
orlyohreally <orli-knop@ya.ru>
2019-03-08 15:03:51 +0300

    first commit

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/randomizer'));
+app.use(express.static(__dirname + '/dist/randomiser'));
 
 app.get('/*', function(req,res) {
     
     
-res.sendFile(path.join(__dirname+'/dist/randomizer/index.html'));
+res.sendFile(path.join(__dirname+'/dist/randomiser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

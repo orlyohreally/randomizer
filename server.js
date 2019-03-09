928044247dfbb17acb3ab19739fd68c1d5dacd36
orlyohreally <orli-knop@ya.ru>
2019-03-09 23:50:57 +0300

    1. mongodb (mongoosejs) setup 2. routing to get phrases

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const mongoose = require('mongoose');
 const app = express();
 
-// Serve only the static files form the dist directory
+//Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
@@ -14,3 +14,33 @@ res.sendFile(path.join(__dirname+'/dist/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+console.log("Listening on port ", process.env.PORT || 8080);
+
+var db = mongoose.connect('mongodb://localhost:27017/randomizer');
+phraseSchema = createPhrasesCollection();
+Phrase = mongoose.model('Phrase', phraseSchema);
+//createPhrases();
+
+function createPhrasesCollection() {
+	return new mongoose.Schema({
+		id: Number,
+		text: String
+	});
+}
+function createPhrases() {
+	var newPhrase = new Phrase({id: 1, text: "text testing"});
+	newPhrase.save(function(err, res) {
+		console.log(err, res);
+	});
+}
+// routing
+app.route('/api/phrases').get((req, res) => {
+	Phrase.find(function(err, result) {
+		if(err) {
+			return console.error(err);
+		}
+		console.log(result);
+		res.send(result);
+	})
+
+});
